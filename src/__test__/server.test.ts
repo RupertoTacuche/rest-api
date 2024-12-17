395df469c8ca2e85a3eb56d2da2706b94c53b67c
@RupertoTacuche <murcio2490@gmail.com>
2024-12-16 23:52:16 -0600

    clase 377 funcionando

--- src/__test__/server.test.ts
+++ src/__test__/server.test.ts
@@ -4,6 +4,11 @@ import server from '../server'
 describe('GET /api', () => {
     it ('should send back a json response', async () => {
         const res = await request(server).get('/api')
-        console.log(res)
+
+        expect(res.status).toBe(200)
+        expect(res.headers['content-type']).toMatch(/json/)
+        expect(res.body.msg).toBe('Desde API')
+        //console.log(res.text)
+        //console.log(res.body.msg)
     })
 })
